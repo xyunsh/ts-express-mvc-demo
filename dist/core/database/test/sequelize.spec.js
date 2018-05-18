134d9e70ef32dcd901e5bb879b22448efaf760c0
shenyj <xyunsh@gmail.com>
2018-05-18 12:37:25 +0800

    delete dist

--- dist/core/database/test/sequelize.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require("../");
-_1.Actor.findAll({ raw: true }).then(actors => {
-    console.log('actors===============>', actors);
-});
-//# sourceMappingURL=sequelize.spec.js.map
